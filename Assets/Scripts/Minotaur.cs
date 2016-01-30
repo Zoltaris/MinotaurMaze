4b7698ac4ef6b4f6400d83a93cf53218c1a3453e
Glenn Davidson <panloaf@gmail.com>
2016-01-30 16:51:10 +0000

    Add Minotaur movement mimicking player

---
+++ Assets/Scripts/Minotaur.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minotaur : MonoBehaviour {
+
+	public AudioClip _cantMove;
+	public AudioClip _Footsteps;
+
+	AudioSource audio;
+
+	private MazeCell currentCell;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+	}
+
+	public void SetLocation (MazeCell cell)
+	{
+		currentCell = cell;
+		transform.localPosition = cell.transform.localPosition;
+	}
+
+	private void Move (MazeDirection direction)
+	{
+		MazeCellEdge edge = currentCell.GetEdge(direction);
+		if (edge is MazePassage)
+		{
+			SetLocation(edge.otherCell);
+			GetComponent<AudioSource>().PlayOneShot(_Footsteps, 1f);
+		}
+		else
+		{ 
+			GetComponent<AudioSource>().PlayOneShot(_cantMove, 1f); 
+		}    
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Move(MazeDirection.North);
+		}
+		else if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			Move(MazeDirection.East);
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Move(MazeDirection.South);
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			Move(MazeDirection.West);
+		}
+	}
+}
