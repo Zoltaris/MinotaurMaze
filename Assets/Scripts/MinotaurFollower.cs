f0c3539ecdec122a54616427c36d112c1919e34f
Zoltaris <andrew.sheppard93@hotmail.co.uk>
2016-01-31 03:31:49 +0000

    Addition of Background Music, editing of Minotaur to include death noise.

--- Assets/Scripts/MinotaurFollower.cs
+++ Assets/Scripts/MinotaurFollower.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class MinotaurFollower : MonoBehaviour {
 
-	public AudioClip _cantMove;
-	public AudioClip _Footsteps;
 
-	AudioSource audioSource;
+    public AudioClip _Dead;
+
+	AudioSource audio;
 
 	private MazeCell currentCell;
 
 	// Use this for initialization
 	void Start () {
-		audioSource = GetComponent<AudioSource>();
+		audio = GetComponent<AudioSource>();
 	}
 
 	public void SetLocation (MazeCell cell)
@@ -27,16 +27,24 @@ public class MinotaurFollower : MonoBehaviour {
 		if (edge is MazePassage)
 		{
 			SetLocation(edge.otherCell);
-			GetComponent<AudioSource>().PlayOneShot(_Footsteps, 1f);
+			
 		}
-		else
-		{ 
-			GetComponent<AudioSource>().PlayOneShot(_cantMove, 1f); 
-		}    
+	   
 	}
 
-	// Update is called once per frame
-	void Update () {
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            audio.PlayOneShot(_Dead, 1f);
+            
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow))
 		{
 			Move(MazeDirection.North);
